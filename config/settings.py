292557125b28686f014df7175e857cffc5cccc72
jiwonman <kusa1230@naver.com>
2024-04-26 00:21:09 +0900

    Feat: 회원가입 기능 구현

--- config/settings.py
+++ config/settings.py
@@ -138,5 +138,12 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
 }
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from users import views 
 
 urlpatterns = [
+    path('signup/', views.SignUpAPIView.as_view(), name='signup')
 ]

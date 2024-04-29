687f2050aee1537495f4045b2b26998e00046f8b
jiwonman <kusa1230@naver.com>
2024-04-29 22:50:42 +0900

    Hotfix: 환경변수 설정 사라져서 급히 추가

--- config/settings.py
+++ config/settings.py
@@ -1,17 +1,17 @@
+import os
+import environ
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!!3(sttq*+zmrawh@y0esy2cj%7mth6w(hjbu$-kx7z=3r_dgt"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
